dc8f9f8df2d4e1d8bbe83e1bc882938e88dffa27
Jamie Tartell <jetartell@gmail.com>
2020-02-24 19:05:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,21 +4,16 @@ function myAnimal() {
   // You should not need to modify this function
   return animal
 }
+myAnimal()
 
 function yourAnimal() {
-  // The tests expect this function to return `animal` just like the previous function
-  // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: Hard-coding 'cat' below will not work
+  var animal = 'cat'
   return animal
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + 2
 }
 
 var funkyFunction = function() {
@@ -27,6 +22,32 @@ var funkyFunction = function() {
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+
+var theFunk = funkyFunction()
+
+
+function funkyFunction() {
+  var theFunk = "FUNKY!"
+  return function inner(theFunk) {
+      return theFunk
+  }
+}
+
+funkyFunction()
+var greeting = funkyFunction()
+greeting('FUNKY!')
+
+
+  describe('funkyFunction()', function() {
+    it('returns a function', function() {
+      expect(typeof window.funkyFunction()).toEqual('function')
+    })
+  })
+
+  describe("the variable 'theFunk'", function() {
+    it('is equal to "FUNKY!"', function() {
+      expect(window.theFunk).toEqual('FUNKY!')
+    })
+  })
+
+})
